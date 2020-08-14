1aa9a789eded87ecf98ef6d9cc8af56a3ba2da60
ltsimps <ltsimpso@me.com>
2020-08-14 10:37:58 -0400

    Setup

---
+++ lib/features/insights/presentation/bloc/insights_event.dart
@@ -0,0 +1,3 @@
+part of 'insights_bloc.dart';
+@immutable
+abstract class InsightsEvent {}
---
+++ lib/features/insights/presentation/bloc/insights_state.dart
@@ -0,0 +1,4 @@
+part of 'insights_bloc.dart';
+@immutable
+abstract class InsightsState {}
+class InsightsInitial extends InsightsState {}
